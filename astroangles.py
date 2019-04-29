3621cc03bf4a19ff3f0a69573317dd58ac6bd02c
Josh Bicking <jhb2345@rit.edu>
2019-04-29 09:25:30 -0400

    add spdx header, unify on gpl 2.0+, remove "how to" in license

--- astroangles.py
+++ astroangles.py
@@ -12,20 +12,8 @@
 # Copyright (C) 2019  Giovanni Aleman
 # Copyright (C) 2019  Derek Erway
 
-#  This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Code:   git://github.com/jibby0/IGME582-final.git
+# Code:   https://github.com/jibby0/IGME582-final
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 import math
 from random import sample
